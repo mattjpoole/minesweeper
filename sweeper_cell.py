8e00426551ef4ac2ebd95a745e20ca7df262cef9
Matt Poole <mattjpoole@hotmail.com>
2024-01-26 22:20:37 +0000

    Game recurses over cells if no mines nearby Not fully working yet, not all cells revealed.

--- sweeper_cell.py
+++ sweeper_cell.py
@@ -1,5 +1,5 @@
 import pygame
-from config import ICON_STATE_CLOSED, ICON_FLAG
+from config import ICON_STATE_CLOSED, ICON_FLAG, ICON_STATE_PENDING, ICON_STATE_EMPTY
 
 class SweeperCell():
 
@@ -10,6 +10,8 @@ class SweeperCell():
         self.state = state
         self.icon = None
         self.size = None
+        self.column = 0
+        self.row = 0
 
     def get_rect(self) -> pygame.Rect:
         return self.rect
@@ -25,6 +27,12 @@ class SweeperCell():
     
     def is_closed(self) -> bool:
         return self.state == ICON_STATE_CLOSED
+
+    def is_empty(self) -> bool:
+        return self.state == ICON_STATE_EMPTY
+    
+    def state_is_pending(self) -> bool:
+        return self.state == ICON_STATE_PENDING
     
     def has_flag(self) -> bool:
         return self.state == ICON_FLAG
@@ -35,8 +43,18 @@ class SweeperCell():
 
     def set_size(self, size:tuple) -> None:
         self.size = size
+
+    def set_field_coords(self, column, row):
+        self.column = column
+        self.row = row
+   
+    def get_column(self)->int:
+        return self.column
+    
+    def get_row(self)->int:
+        return self.row
     
     def render(self) -> None:
-        if not self.is_closed():
+        if not self.is_closed() and not self.is_empty():
             pygame.display.get_surface().blit(
                 pygame.transform.scale(self.icon, self.size), self.rect)
--- sweeper_field.py
+++ sweeper_field.py
@@ -8,24 +8,26 @@ class SweeperField():
     def __init__(self) -> None:
         self.grid_list = []
         self.icons = []
+        self.grid_size = 0
+        self.rn = 0
 
     def initialise(self, level) -> None:
         """Create a feild of sweeper cells ready to start the game"""
         if level == LEVEL_EASY:
-            grid_size = EASY_GRID_SIZE
+            self.grid_size = EASY_GRID_SIZE
             cell_size = EASY_CELL_SIZE
             num_mines = NUM_MINES_EASY
         elif level == LEVEL_MEDIUM:
-            grid_size = MEDIUM_GRID_SIZE
+            self.grid_size = MEDIUM_GRID_SIZE
             cell_size = MEDIUM_CELL_SIZE
             num_mines = NUM_MINES_MEDIUM
         else:
-            grid_size = HARD_GRID_SIZE
+            self.grid_size = HARD_GRID_SIZE
             cell_size = HARD_CELL_SIZE
             num_mines = NUM_MINES_HARD
 
         self.icons = []
-        total = grid_size * grid_size
+        total = self.grid_size * self.grid_size
         screen = pygame.display.get_surface()
         top = UI_HEIGHT
         column = left = cell_num = row = 0
@@ -48,10 +50,11 @@ class SweeperField():
                     has_mine = True
             cell = SweeperCell(rect, colour, has_mine)
             cell.set_size((cell_size, cell_size))
+            cell.set_field_coords(column, row)
             self.grid_list.append(cell)
             # prepare for next loop
             cell_num += 1
-            column = cell_num % grid_size
+            column = cell_num % self.grid_size
             if column == 0:
                 row += 1
             left = column * cell_size
@@ -65,7 +68,7 @@ class SweeperField():
         for cell in self.grid_list:
             cell_rect = cell.get_rect()
             screen.fill(cell.get_colour(), cell_rect)
-            if (cell_rect.collidepoint(pygame.mouse.get_pos()) and cell.is_closed()):
+            if (cell_rect.collidepoint(pygame.mouse.get_pos()) and cell.is_closed() and not cell.is_empty()):
                 pygame.draw.rect(screen, CELL_BORDER_HOVER, cell_rect, 2)
             cell.render()
 
@@ -78,6 +81,8 @@ class SweeperField():
         return mine_locations
     
     def click_cell_at_coords(self, coords) -> None:
+        index = 0
+
         for cell in self.grid_list:
             cell_rect = cell.get_rect()
             if cell_rect.collidepoint(coords):
@@ -87,8 +92,10 @@ class SweeperField():
                     cell.set_state(ICON_MINE, self.icons[ICON_MINE])
                     self.reveal_all_mines()
                 else:
-                    # iterate over icons based upon neighbourng cells
-                    pass
+                    self.count_adjacent_mines(cell, self.get_neighbours(index))
+                    self.reset_cell_pending_state()
+                    break
+            index += 1
     
     def right_click_cell_at_coords(self, coords) -> None:
         for cell in self.grid_list:
@@ -98,9 +105,108 @@ class SweeperField():
                     cell.set_state(ICON_STATE_CLOSED)
                 elif cell.is_closed():
                     cell.set_state(ICON_FLAG, self.icons[ICON_FLAG])
-                    
+
+    def get_neighbours(self, cell_index) -> list:
+        """Method to select neighbouring cells for give cell"""
+        out_of_bounds_rect = pygame.Rect(0,0,0,0)
+        top_left = top_middle = top_right = middle_left = middle_right = bottom_left = bottom_middle = bottom_right = SweeperCell(out_of_bounds_rect, None, False, ICON_STATE_OUTOFBOUNDS)
+        kernel = self.grid_list[cell_index]
+        try:
+            # if we are not at the top edge
+            if kernel.get_row()>0:
+                top_middle = self.grid_list[cell_index-self.grid_size]
+                # and if we are not at the left edge
+                if kernel.get_column()>0:
+                    top_left = self.grid_list[cell_index-self.grid_size-1]
+                # and if we are not at the right edge
+                if kernel.get_column()<self.grid_size-1:
+                    top_right = self.grid_list[cell_index-self.grid_size+1]
+            
+            # if we are not on the left edge
+            if kernel.get_column()>0:
+                middle_left = self.grid_list[cell_index-1]
+             
+            # if we are not on the right edge
+            if kernel.get_column()<self.grid_size-1:
+                middle_right = self.grid_list[cell_index+1]
+            
+            # if we are not the bottom edge
+            if kernel.get_row() < self.grid_size-1:
+                bottom_middle = self.grid_list[cell_index+self.grid_size]
+                # and we are not on the left edge
+                if kernel.get_column()>0:
+                    bottom_left = self.grid_list[cell_index+self.grid_size-1]
+                # and we are not at the right edge
+                if kernel.get_column()<self.grid_size-1:
+                    bottom_right = self.grid_list[cell_index+self.grid_size+1]
+
+        except IndexError:
+            print("cell index out of bounds ====")
+            print("kernel.get_column()"+str(kernel.get_column()))
+            print("kernel.get_row()"+str(kernel.get_row()))
+        neighbours = [top_left, top_middle, top_right, middle_left, middle_right, bottom_left, bottom_middle, bottom_right]
+        return neighbours
+    
+    def count_adjacent_mines(self, cell, neighbours:list) -> None:
+        mines = 0
+        new_neighbours = []
+        for neighbour in neighbours:
+            if neighbour.is_mine_square():
+                mines += 1
+            elif neighbour.is_closed():
+                # the pending state means it wont be checked more than once.
+                neighbour.set_state(ICON_STATE_PENDING)
+                new_neighbours.append(neighbour)
+
+        if mines > 0:
+            # set the number icon for this cell
+            cell_state = ICONS[mines-1]
+            cell.set_state(cell_state, self.icons[cell_state])
+        else:
+            cell.set_state(ICON_STATE_EMPTY)
+            cell.set_colour(CELL_COLOUR_EMPTY)
+            for neighbour in new_neighbours:
+                n_index = self.get_grid_index_from_cell(neighbour)
+                self.count_adjacent_mines(neighbour, self.get_neighbours(n_index))
+
+    def count_adjacent_mines_p(self, cell, neighbours:list) -> None:
+        new_neighbours = self.set_num_mines(cell, neighbours)
+        if len(new_neighbours) > 0:
+            # check the neightbours of this cell for the number of adjacent mines
+            for neighbour in new_neighbours:
+                # the pending state means it wont be checked more than once.
+                n_index = self.get_grid_index_from_cell(neighbour)
+                next_neighbours = self.set_num_mines(neighbour, self.get_neighbours(n_index))
+                for n in next_neighbours:
+                    self.set_num_mines(n, next_neighbours)
+    
+    def set_num_mines(self, cell, neighbours) -> list:
+        mines = 0
+        new_neighbours = []
+        cell.set_state(ICON_STATE_PENDING)
+        for neighbour in neighbours:
+            if neighbour.is_mine_square():
+                mines += 1
+            elif neighbour.is_closed():
+                neighbour.set_state(ICON_STATE_PENDING)
+                new_neighbours.append(neighbour)
+
+        if mines > 0:
+            # set the number icon for this cell
+            cell_state = ICONS[mines-1]
+            cell.set_state(cell_state, self.icons[cell_state])
+        return new_neighbours
+
+    def get_grid_index_from_cell(self, cell) -> int:
+        index = cell.get_column() + (cell.get_row() * self.grid_size) 
+        return index       
     
     def reveal_all_mines(self):
         for cell in self.grid_list:
             if cell.is_mine_square() and cell.is_closed:
                 cell.set_state(ICON_MINE, self.icons[ICON_MINE])
+    
+    def reset_cell_pending_state(self) -> None:
+        for cell in self.grid_list:
+            if cell.state_is_pending():
+                cell.set_state(ICON_STATE_CLOSED)
